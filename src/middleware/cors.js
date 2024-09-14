4ea6ba7f4425636d1efc385f9b5dd027c3570168
Somkene Ojukwu <somkeneoj@gmail.com>
2024-09-14 04:13:23 +0100

    cors config for frontend url

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -2,7 +2,7 @@ import cors from 'cors';
 
 // Create and export the CORS middleware configuration
 const corsMiddleware = cors({
-  origin: 'http://localhost:3000', // Replace with your frontend URL
+  origin: `${process.env.VERCEL_FRONTEND_URL}`, // Replace with your frontend URL
   credentials: true, // Allow cookies to be sent
   optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
 });
